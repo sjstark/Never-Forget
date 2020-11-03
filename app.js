895e7654234d2e32e72bf7a1ba36f574da479609
brandon-perry <brandonperryofficial@gmail.com>
2020-11-03 11:19:56 -0500

    end of day 1

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const { restoreUser } = require('./auth')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const taskRouter = require('./routes/tasks')
 
 const app = express();
 
@@ -47,6 +48,7 @@ store.sync();
 app.use(restoreUser)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks',taskRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator')
 const { csrfProtection, asyncHandler } = require('./utils')
+const cors = require('cors')
+const {Task} = require('../db/models')
+
+
+
 const router = express.Router();
 
 
+router.get('/', csrfProtection, asyncHandler(async (req,res) => {
+    let allTasks = await Task.findAll();
+    // console.log(allTasks)
+
+    res.render('dummy-all-tasks', {
+        title: 'Dummy',
+        allTasks,
+        csrfToken: req.csrfToken()
+    })
+}))
+
+
+
+module.exports = router
