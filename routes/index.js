1a178982c57a31acf3964bc284bc374aac9fafe4
Sam Stark <SamStarkScenic@gmail.com>
2020-11-02 22:01:19 -0500

    Navbar started for app view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
+router.get('/app/test', (req,res) => {
+  res.render('app', {title: 'Never Forget'})
+})
+
 module.exports = router;
