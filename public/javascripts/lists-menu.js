4434e1132d0727b596934268c57c6ff508c80cdd
brandon-perry <brandonperryofficial@gmail.com>
2020-11-04 16:38:01 -0500

    make default for nested active, implemented tree view for inbox

--- public/javascripts/lists-menu.js
+++ public/javascripts/lists-menu.js
@@ -16,20 +16,37 @@ document.addEventListener('DOMContentLoaded', ()=> {
     //         el.classList.toggle('caret-down')
     //     })
     // })
-    console.log('hit1')
-    console.log(caretToggle[0])
-    caretToggle[0].addEventListener('click', ()=> {
-        console.log('hit2')
-        // let parentEl = el.parentElement.querySelector('.nested');
-        let parentTreeDOM = caretToggle[0].parentElement.parentElement;
-        console.log(parentTreeDOM)
-        console.log('hit3')
-        let nestedDOM = parentTreeDOM.getElementsByClassName('nested')
-        console.log(nestedDOM)
-        nestedDOM[0].classList.toggle('active')
 
-        caretToggle[0].classList.toggle('caret-down')
+    caretToggle.forEach(caret => {
+        console.log('hit1')
+        console.log(caret)
+        caret.addEventListener('click', ()=> {
+            console.log('hit2')
+            // let parentEl = el.parentElement.querySelector('.nested');
+            let parentTreeDOM = caret.parentElement.parentElement;
+            console.log(parentTreeDOM)
+            console.log('hit3')
+            let nestedDOM = parentTreeDOM.getElementsByClassName('nested')
+            console.log(nestedDOM)
+            nestedDOM[0].classList.toggle('nested--inactive')
+
+            caret.classList.toggle('caret-down')
     })
+    })
+    // console.log('hit1')
+    // console.log(caretToggle[0])
+    // caretToggle[0].addEventListener('click', ()=> {
+    //     console.log('hit2')
+    //     // let parentEl = el.parentElement.querySelector('.nested');
+    //     let parentTreeDOM = caretToggle[0].parentElement.parentElement;
+    //     console.log(parentTreeDOM)
+    //     console.log('hit3')
+    //     let nestedDOM = parentTreeDOM.getElementsByClassName('nested')
+    //     console.log(nestedDOM)
+    //     nestedDOM[0].classList.toggle('active')
+
+    //     caretToggle[0].classList.toggle('caret-down')
+    // })
 
 
 })
