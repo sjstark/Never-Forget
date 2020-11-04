c18bb4d98da73db9b4a34e0131c5673485b44070
brandon-perry <brandonperryofficial@gmail.com>
2020-11-04 15:46:04 -0500

    add list-menu javascript file, add basic content for list menu, and add collapsable tree element for lists

---
+++ public/javascripts/lists-menu.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    let caretToggle = document.getElementsByClassName('caret');
+
+    caretToggle = Array.from(caretToggle)
+    
+    console.log(caretToggle)
+    
+    // caretToggle.forEach(el, ()=> {
+    //     el.addEventListener('click', ()=> {
+    //         console.log(el)
+    //         let parentEl = el.parentElement.querySelector('.nested');
+    //         parentEl.classList.toggle('.active')
+    
+    //         el.classList.toggle('caret-down')
+    //     })
+    // })
+    console.log('hit1')
+    console.log(caretToggle[0])
+    caretToggle[0].addEventListener('click', ()=> {
+        console.log('hit2')
+        // let parentEl = el.parentElement.querySelector('.nested');
+        let parentTreeDOM = caretToggle[0].parentElement.parentElement;
+        console.log(parentTreeDOM)
+        console.log('hit3')
+        let nestedDOM = parentTreeDOM.getElementsByClassName('nested')
+        console.log(nestedDOM)
+        nestedDOM[0].classList.toggle('active')
+
+        caretToggle[0].classList.toggle('caret-down')
+    })
+
+
+})
+
