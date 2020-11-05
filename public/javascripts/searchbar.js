552a0dc1aca862b4afef244f027e72e24c9070a2
Sam Stark <SamStarkScenic@gmail.com>
2020-11-05 18:41:49 -0500

    added enter/escape detection on search input

--- public/javascripts/searchbar.js
+++ public/javascripts/searchbar.js
@@ -1,10 +1,43 @@
-const searchInput = document.querySelector('#searchBar')
-const searchLabel = document.querySelector('.navbar__search-label')
 
-searchInput.addEventListener('focusin', (e) => {
-  searchLabel.style.backgroundColor = "rgba(255,255,255,1)"
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const searchInput = document.querySelector('#searchBar')
+  const searchLabel = document.querySelector('.navbar__search-label')
+
+  searchInput.addEventListener('focusin', (e) => {
+    searchLabel.style.backgroundColor = "rgba(255,255,255,1)"
+  })
+
+  searchInput.addEventListener('focusout', (e) => {
+    searchLabel.style.backgroundColor = "rgba(255,255,255,.2)"
+    searchInput.value = ''
+  })
+
+  searchInput.addEventListener('keyup', async (e) => {
+    console.log('key:', e.key)
+    if (e.key === 'Escape') {
+      clearSearchInput(e);
+      return
+    }
+
+    if (e.key === 'Return') {
+      let searchInput = e.target.value
+      let tasks = await findTasksWithSearch(searchInput)
+      console.log(tasks)
+    }
+
+  })
+
+  const clearSearchInput = (e) => {
+    e.target.value = ''
+    e.target.blur()
+  }
+
 })
 
-searchInput.addEventListener('focusout', (e) => {
-  searchLabel.style.backgroundColor = "rgba(255,255,255,.2)"
-})
+const findTasksWithSearch = async (include = '', exclude = '') => {
+
+
+
+}
