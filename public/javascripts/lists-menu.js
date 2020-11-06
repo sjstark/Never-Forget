5f46a17340d027e3da2f3e3cc754dd39bb8ed9e4
brandon-perry <brandonperryofficial@gmail.com>
2020-11-05 22:41:04 -0500

    implemented dynamic list generator, task counter, and other design tweaks

--- public/javascripts/lists-menu.js
+++ public/javascripts/lists-menu.js
@@ -1,5 +1,9 @@
 
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', async ()=> {
+
+    //-------ADD LISTS THAT THE PERSON CAN ACCESS IN THE DOM------//
+    await loadLists()
+
 
     // ------HANDLE TOGGLE FOR TREE VIEWS-----//
     treeView()
@@ -7,6 +11,14 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     //-------HANDLE EMPHASIS ON SELECTED LISTS AND SECTIONS------//
     emphasisText()
+
+
+    // addLists()
+
+    await countTotalTasks()
+
+    await countListTasks()
+
 
 })
 
@@ -23,7 +35,7 @@ function treeView() {
             // console.log('hit2')
             // let parentEl = el.parentElement.querySelector('.nested');
             let parentTreeDOM = caret.parentElement.parentElement;
-            console.log(parentTreeDOM)
+            // console.log(parentTreeDOM)
             // console.log('hit3')
             let nestedDOM = parentTreeDOM.getElementsByClassName('nested')
             // console.log(nestedDOM)
@@ -35,20 +47,19 @@ function treeView() {
  
 }
 
-
 function emphasisText() {
     let listMenu = document.querySelector('.list-menu');
 
     let texts = document.querySelectorAll('li');
 
-    console.log(texts);
+    // console.log(texts);
 
     let textList = [...texts];
 
     textList.forEach(el => {
-        console.log('reached for each emphasisText')
+        // console.log('reached for each emphasisText')
         el.addEventListener('click', ()=> {
-            console.log('reached event listener')
+            // console.log('reached event listener')
             emphasisHelperFunction(textList, el)
         })
     })
@@ -56,9 +67,106 @@ function emphasisText() {
 
 function emphasisHelperFunction(textList, el) {
     textList.forEach(item => {
-        console.log('reached for each helper function')
+        // console.log('reached for each helper function')
         item.classList.remove('list-tree-li--emphasis')
     })
-    console.log('got to end of helper function')
+    // console.log('got to end of helper function')
     el.classList.add('list-tree-li--emphasis')
+}
+
+async function loadLists() {
+    let route = '/lists'
+
+    //Fetching and uppacking response
+    let res = await fetch(route);
+    let resObj = await res.json();
+    let objArray = resObj.allLists
+
+    //Finding list of lists in menu and clearing it
+    let listTree = document.querySelector('#add-lists-here')
+    listTree.innerHTML = '';
+    
+
+    //Adding lists to tree
+    objArray.forEach(list => {
+        // console.log(list)
+        let htmlList = document.createElement('li')
+        htmlList.classList.add('list-menu-flex')
+        htmlList.id = list.id
+        htmlList.innerHTML = `
+        ${list.title} 
+        <span class="listCount" id='${list.id}'></span>
+        <span class='list-edit-carrot'>V</span>`
+        listTree.appendChild(htmlList) 
+    }) 
+}
+
+// async function addLists() {
+//     //Find add button in DOM
+//     let addButton = document.querySelector('.add-list-button');
+    
+//     addButton.addEventListener('click', (event) => {
+//         event.stopPropagation()
+
+//         addListsHelperFunction()
+//     })  
+
+// };
+
+// function addListsHelperFunction() {
+//     let route = 
+// }
+
+async function countTotalTasks() {
+    //Grab All Tasks span
+    let taskCount = document.querySelector('.allTaskCount');
+    // console.log(taskCount)
+    // console.log(taskCount.innerHTML)
+
+    //Fetch and parse request from API
+    let route = '/tasks'
+
+    let req = await fetch(route)
+    let res = await req.json();
+    let taskArray = res.allTasks
+    // console.log(taskArray)
+
+    let count = taskArray.length
+    // console.log(count)
+
+    taskCount.innerText = count
+}
+
+async function countListTasks() {
+    //Grab all lists count spans
+    let listCounts = document.querySelectorAll('.listCount');
+
+    let listCountsArray = [...listCounts]
+
+    //Iterate through spans, grab their list.length, and set span
+    listCountsArray.forEach(async (el) => {
+        let listId = el.id;
+        // console.log(el)
+        let route = `lists/${listId}`
+
+        let req = await fetch(route)
+        let res = await req.json()
+        // console.log(res)
+        let {allTasks} = res
+        // console.log(allTasks)
+
+        let count = allTasks.length
+        // console.log(count)
+
+        el.innerHTML = count;
+
+    })
+}
+
+
+module.exports = {
+    countTotalTasks,
+    countListTasks,
+    emphasisHelperFunction,
+    loadLists
 }
