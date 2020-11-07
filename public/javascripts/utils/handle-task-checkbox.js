47577310763bb352ce62496d138cc6f68e9b65e3
Sam Stark <SamStarkScenic@gmail.com>
2020-11-07 17:45:40 -0500

    updated list summary with completing tasks

--- public/javascripts/utils/handle-task-checkbox.js
+++ public/javascripts/utils/handle-task-checkbox.js
@@ -1,9 +1,10 @@
 import {updateTask} from './showTaskDetails.js'
+import { updateTaskSummary } from './updateTaskSummary.js';
 
 export const handleChecking = (checkBox, task) => {
   if (task.isComplete) checkBox.checked = true;
 
-  checkBox.onclick = (e) => {
+  checkBox.onclick = async (e) => {
     e.stopPropagation();
 
     checkBox.disabled = true;
@@ -29,9 +30,37 @@ export const handleChecking = (checkBox, task) => {
 
 
 
-    updateTask(task.id, 'isComplete', checkBox.checked)
+    await updateTask(task.id, 'isComplete', checkBox.checked)
+    await updateThisListSummary()
     setTimeout(() => {
       deadListItem.classList.add('task-deleted--off')
     }, 150)
   }
+}
+
+const updateThisListSummary = async () => {
+
+  let listId = localStorage.getItem("never-forget-currentList")
+    ? localStorage.getItem("never-forget-currentList")
+    : null;
+  if (listId === "null") listId = null;
+
+  let route = "/tasks";
+  if (listId) {
+    if (listId.startsWith('search:')){
+      let searchInput = listId.slice(7)
+      route = `/tasks/search?includes=${encodeURI(searchInput)}`
+    } else {
+      route = `/lists/${listId}`
+    }
+  }
+
+  let res = await fetch(route);
+  let body = await res.json();
+
+  let tasks = body.allTasks;
+
+  //update task summary
+  updateTaskSummary(tasks);
+
 }
