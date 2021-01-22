d23a9f732b48ea365e1d84cd41be445cf23306f0
Miguel Munoz <memg92@gmail.com>
2021-01-22 13:31:31 -0300

    demo user button on login page now logs the user in and redirects to /app

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,25 +1,25 @@
-let demoButton = document.querySelector('#demobtn')
+let demoButton = document.querySelector("#demobtn");
 
-demoButton.addEventListener('click', (e)=> {
-
+demoButton.addEventListener("click", async (e) => {
   e.preventDefault();
 
-  fillForm()
-})
+  fillForm();
+
+  await fetch("/users/demo");
+  window.location.href = "/app";
+});
 
 const fillForm = async () => {
   const demoUser = {
-    email: 'demo@user.com',
-    password: 'P@ssw0rd'
-  }
+    email: "demo@user.com",
+    password: "P@ssw0rd",
+  };
 
-  for(let key in demoUser) {
-
-    let value = demoUser[key]
-    let field = document.querySelector(`#${key}`)
-    for(let i = 0; i <= value.length; i++) {
-      field.value = value.slice(0,i)
+  for (let key in demoUser) {
+    let value = demoUser[key];
+    let field = document.querySelector(`#${key}`);
+    for (let i = 0; i <= value.length; i++) {
+      field.value = value.slice(0, i);
     }
-
   }
-}
+};
